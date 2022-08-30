9e558c872797bb21cf6f63016b7dee141189cae3
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-08-30 13:51:32 -0700

    final touches

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,22 +10,11 @@ function Contact() {
     <div style={{backgroundColor: 'black', color: 'white', marginBottom: '0px', fontFamily: 'roboto'}}>
     <Container style={{width:'100vh', height: '100vh'}}>
     <Row className="row justify-content-between">
-      <h1>Want to get in touch?</h1>
-      <a href="mailTo:matthewtlarson@arizona.edu">✉</a>
-      <Form style={{width: '75%'}}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          Response within 1 business day.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Message</Form.Label>
-        <Form.Control type="text" placeholder="I am messaging because..." />
-      </Form.Group>
-      <Button variant="outline-success">Send!</Button>
-    </Form>
+      <h1 style={{fontStyle:'italic', marginTop: '20px', fontSize: '50px'}}>Want to get in touch?</h1><h4>Phone 📞: 650-400-1884</h4>
+      <button class="glow-on-hover" type="button" style={{width: '120px', marginTop: '90px'}}><a href="mailTo:matthewtlarson@arizona.edu" style={{color: 'white', fontSize: '20px' }}>Email ✉</a></button>
+      <button class="glow-on-hover" type="button" style={{width: '120px', marginTop: '90px'}}><a href="https://www.linkedin.com/in/matthew-larson51/" target="_" style={{color: 'white', fontSize: '20px' }}> LinkedIn </a></button>
+      <button class="glow-on-hover" type="button" style={{width: '120px', marginTop: '90px'}}><a href="https://github.com/matthewtlarson" target="_" style={{color: 'white', fontSize: '20px' }}>Github</a></button>
+      <button class="glow-on-hover" type="button" style={{width: '120px', marginTop: '90px'}}><a href="https://www.instagram.com/matt_larson33/?hl=en" target="_" style={{color: 'white', fontSize: '20px' }}>Instagram</a></button>
     </Row>
     </Container>
     </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -13,11 +13,11 @@ function Portfolio() {
       <Card style={{ width: '18rem', marginTop: '20vh', marginRight: '200px' }}>
       <Card.Img variant="top" src={WYD}/>
       <Card.Body>
-        <Card.Title style={{color: 'black'}}>WYD</Card.Title>
+        <Card.Title style={{color: 'black', fontSize: '30px', fontWeight: '700'}}>WYD</Card.Title>
         <Card.Text style={{color: 'black'}}>
         An application to make event planning and coordination much easier for all events from small-group hangouts to professional, full-scale events.
         </Card.Text>
-        <Button variant="primary" href="https://peaceful-reef-29677.herokuapp.com/" target="_">Check it out!</Button>
+        <Button variant="primary" href="https://peaceful-reef-29677.herokuapp.com/" target="_" style={{backgroundColor: 'black'}}>Check it out!</Button>
       </Card.Body>
     </Card>
       </div>
@@ -25,11 +25,11 @@ function Portfolio() {
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={STOCK} />
       <Card.Body>
-        <Card.Title style={{color: 'black'}}>Market Moment</Card.Title>
+        <Card.Title style={{color: 'black', fontSize: '30px', fontWeight: '700'}}>Market Moment</Card.Title>
         <Card.Text style={{color: 'black'}}>
         This application can be used to keep track of your favorite stocks, stay on top of latest news, and read-up on only the most vital, up-to-date information.
         </Card.Text>
-        <Button variant="primary" href="https://brandtdavidson.github.io/interactive-stock/" target="_">Check it out!</Button>
+        <Button variant="primary" href="https://brandtdavidson.github.io/interactive-stock/" target="_" style={{backgroundColor: 'black'}}>Check it out!</Button>
       </Card.Body>
     </Card>
       </div>
@@ -37,11 +37,11 @@ function Portfolio() {
       <Card style={{ width: '18rem', marginTop: '20vh' }}>
       <Card.Img variant="top" src={DBD} style={{height: '24vh'}} />
       <Card.Body>
-        <Card.Title style={{color: 'black'}}>DayByDay</Card.Title>
+        <Card.Title style={{color: 'black', fontSize: '30px', fontWeight: '700'}}>DayByDay</Card.Title>
         <Card.Text style={{color: 'black'}}>
         The DayByDay application was created with the intention to help motivate, track, and urge you to stay consistent in a specific area of your life.
         </Card.Text>
-        <Button variant="primary" href="https://daybyday-application.herokuapp.com/login" target="_">Check it out!</Button>
+        <Button variant="primary" href="https://daybyday-application.herokuapp.com/login" target="_" style={{backgroundColor: 'black'}}>Check it out!</Button>
       </Card.Body>
     </Card>
       </div>
